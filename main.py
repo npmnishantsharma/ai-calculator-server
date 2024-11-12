4fd2e18df5fbf48b443c8803bd92b47e27b30f72
Nishant Sharma <apsanishat@gmail.com>
2024-11-12 22:12:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
